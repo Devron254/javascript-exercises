b83a61119c768e3f0d856d4b08e7579361d11674
Devron254 <125981992+Devron254@users.noreply.github.com>
2026-04-20 05:57:37 +0300

    Corrected reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+function reverseString(string) {
+  let result = "";
 
-};
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+
+  return result;
+}
+
+module.exports = reverseString;
 
 // Do not edit below this line
 module.exports = reverseString;
