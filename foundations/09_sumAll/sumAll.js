660a139158364dd11661a0ac401c6597b1764e72
Devron254 <125981992+Devron254@users.noreply.github.com>
2026-04-21 22:27:45 +0300

    Corrected sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+function sumAll(a,b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'ERROR';
+  }
 
-};
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
+
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return 'ERROR';
+  }
+
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
