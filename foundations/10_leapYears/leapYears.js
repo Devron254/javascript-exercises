09f28fb3ec9e25ed3b39c3658fddfaf9e89818f1
Devron254 <125981992+Devron254@users.noreply.github.com>
2026-04-22 22:14:57 +0300

    Corrected leapYears exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+function leapYears(year) {
+  if (year % 400 === 0) {
+    return true;
+  }
 
-};
+  if (year % 100 === 0) {
+    return false;
+  }
 
+  if (year % 4 === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = leapYears;
 // Do not edit below this line
 module.exports = leapYears;
