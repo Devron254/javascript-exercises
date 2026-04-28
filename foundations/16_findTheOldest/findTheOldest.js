949819bb772e430986661a0ff67a18ff13540e8d
Devron254 <125981992+Devron254@users.noreply.github.com>
2026-04-28 05:09:21 +0300

    Corrected findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+  let oldest = people[0];
 
-};
+  for (let i = 0; i < people.length; i++) {
+    let person = people[i];
+
+    let deathYear = person.yearOfDeath || new Date().getFullYear();
+
+    let age = deathYear - person.yearOfBirth;
+
+    let oldestDeathYear = oldest.yearOfDeath || new Date().getFullYear();
+    let oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+    if (age > oldestAge) {
+      oldest = person;
+    }
+  }
+
+  return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
